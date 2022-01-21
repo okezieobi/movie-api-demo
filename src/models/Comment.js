81a986b67f2943185e7689f9e93833fcaf178488
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-01-21 16:13:20 +0100

    comment model bug fixed

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -4,27 +4,32 @@ import connect from './connect';
 import AppError from '../errors';
 import CommentSchema from '../schemas/Comment';
 
+const sqlFiles = {
+  createTable: connect.sql('../../sql/comments/table.sql'),
+  insertOne: connect.sql('../../sql/comments/insert.sql'),
+  filterMany: connect.sql('../../sql/comments/filter.sql'),
+  paginate: connect.sql('../../sql/comments/paginate.sql'),
+  paginate_last: connect.sql('../../sql/comments/paginate_last.sql'),
+};
+
 class CommentModel {
   constructor(schemas = { Comment: CommentSchema }) {
     this.schemas = schemas;
     this.getPaginated = this.getPaginated.bind(this);
     this.setupTable = async () => {
-      const sqlFile = connect.sql('../../sql/comments/table.sql');
-      await connect.db.none(sqlFile);
+      await connect.db.none(sqlFiles.createTable);
     };
     this.insertOne = async (data) => {
-      const sqlFile = connect.sql('../../sql/comments/insert.sql');
-      const result = await connect.db.one(sqlFile, data).catch((err) => {
+      const result = await connect.db.one(sqlFiles.insertOne, data).catch((err) => {
         throw new AppError(err.message, 'Query', err);
       });
       return result;
     };
     this.filterMany = async (data) => {
-      const sqlFile = connect.sql('../../sql/comments/filter.sql');
       await connect.db.task(async (t) => {
         await Promise.all(data.map(async (film) => {
           const placeholder = film;
-          placeholder.comments = await t.any(sqlFile, [placeholder.episode_id]);
+          placeholder.comments = await t.any(sqlFiles.filterMany, [placeholder.episode_id]);
           placeholder.comment_count = placeholder.comments.length;
         }));
       });
@@ -33,10 +38,8 @@ class CommentModel {
 
   async getPaginated(id) {
     await new this.schemas.Comment({ id }).validateSelectQuery();
-    const sqlFileOne = connect.sql('../../sql/comments/paginate.sql');
-    const sqlFileTwo = connect.sql('../../sql/comments/paginate_last.sql');
-    if (id != null) return connect.db.any(sqlFileOne, [id]);
-    return connect.db.any(sqlFileTwo);
+    if (id != null) return connect.db.any(sqlFiles.paginate, [id]);
+    return connect.db.any(sqlFiles.paginate_last);
   }
 }
 
